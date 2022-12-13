d551e2ff758ce5dde991f5aeda2bbe65d05b5888
742617000027 <15521878+742617000027@users.noreply.github.com>
2022-12-13 12:42:25 +0100

    refactor using cmp_to_key

--- 13/13.py
+++ 13/13.py
@@ -5,39 +5,27 @@ def preprocess(data):
     return [[eval(packet) for packet in pair.split('\n')] for pair in data.split('\n\n')]
 
 
-def compare(pair):
-    for a, b in zip(*pair):
+def compare(l, r):
+    for a, b in zip(*[l, r]):
         while True:
              match (a, b):
                 case (int(), int()):
-                    if a < b: return True
-                    if b < a: return False
+                    if a < b: return 1
+                    if b < a: return -1
                     break
                 case (list(), list()):
-                    ret = compare([a[:len(b)], b[:len(a)]])
+                    ret = compare(a[:len(b)], b[:len(a)])
                     if ret is not None: return ret
-                    if len(a) < len(b): return True
-                    if len(b) < len(a): return False
+                    if len(a) < len(b): return 1
+                    if len(b) < len(a): return -1
                     break
                 case _:
                     swap = isinstance(a, list)
                     if swap: b, a = a, b
                     a = [a]
                     if swap: a, b = b, a
-    a, b = pair
-    if len(a) < len(b): return True
-    if len(b) < len(a): return False
-
-
-def sort(data):
-    l = data[:]
-    for i, j in zip(range(1, len(l)), range(len(l) - 1)):
-        key = l[i]
-        while j >= 0 and compare([key, l[j]]):
-            l[j+1] = l[j]
-            j -= 1
-        l[j+1] = key
-    return l
+    if len(l) < len(r): return 1
+    if len(r) < len(l): return -1
 
 
 if __name__ == '__main__':
@@ -47,13 +35,15 @@ if __name__ == '__main__':
     """
     timer.start()
     data = preprocess(utils.read())
-    print(sum([i if compare(pair) else 0 for i, pair in enumerate(data, start=1)]))
+    print(sum([i if compare(a, b) > 0 else 0 for i, (a, b) in enumerate(data, start=1)]))
     timer.stop()  # 14.26ms
     """
 
     # Part 2
     timer.start()
     divider = [[[2]], [[6]]]
-    data = preprocess(utils.read().replace('\n\n', '\n')).pop() + divider
-    print(utils.reduce(lambda a, b: a * b, [i for i, elem in enumerate(sort(data), start=1) if elem in divider]))
-    timer.stop()  # 59.21ms
+    data = sorted(preprocess(utils.read().replace('\n\n', '\n')).pop() + divider,
+                  key=utils.cmp_to_key(compare),
+                  reverse=True)
+    print(utils.reduce(lambda a, b: a * b, [i for i, elem in enumerate(data, start=1) if elem in divider]))
+    timer.stop()  # 27.70ms
