be6d50831e14557172ece63e3c50868943972f42
742617000027 <15521878+742617000027@users.noreply.github.com>
2022-12-13 01:29:06 +0100

    remove numpy and refactor

--- 12/12.py
+++ 12/12.py
@@ -1,43 +1,23 @@
-from itertools import product
-
 import networkx as nx
-import numpy as np
 
 import utils
 
 Vector = tuple[int, int]
 
 
-def preprocess(data: list[str]) -> tuple[nx.DiGraph, Vector, Vector]:
-    terrain, start, end = make_map(data)
-    G = make_graph(terrain)
-    return G, start, end
-
-
-def make_map(data: list[str]) -> np.array:
-    terrain = np.array([[ord(c) for c in line] for line in data])
-    start, end = find('S', terrain), find('E', terrain)
-    terrain[start], terrain[end] = ord('a'), ord('z')
-    return terrain, start, end
-
-
-def find(needle: str, haystack: np.array) -> Vector:
-    return tuple(np.argwhere(haystack == ord(needle)).squeeze())
-
-
-def make_graph(terrain: np.array) -> nx.DiGraph:
+def process(data: list[str]) -> tuple[nx.DiGraph, Vector, Vector]:
+    terrain = [[ord(c) for c in line] for line in data]
+    [(sy, sx)], [(ey, ex)] = [[(y, x.index(c)) for y, x in enumerate(terrain) if c in x] for c in [ord('S'), ord('E')]]
+    terrain[sy][sx], terrain[ey][ex] = ord('a'), ord('z')
+    Y, X = len(terrain), len(terrain[0])
     G = nx.DiGraph()
-    Y, X = terrain.shape
 
-    for current in product(range(Y), range(X)):
-        y, x = current
-        G.add_node(current, height=terrain[current])
-        for dy, dx in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-            if 0 <= (yy := y + dy) < Y and 0 <= (xx := x + dx) < X:
-                neighbor = (yy, xx)
-                if terrain[neighbor] - 1 <= terrain[current]:
-                    G.add_edge(current, neighbor)
-    return G
+    for y, x in utils.product(range(Y), range(X)):
+        G.add_node((y, x), height=terrain[y][x])
+        for dy, dx in utils.DIRS:
+            if 0 <= (yy := y + dy) < Y and 0 <= (xx := x + dx) < X and terrain[yy][xx] - 1 <= terrain[y][x]:
+                G.add_edge((y, x), (yy, xx))
+    return G, (sy, sx), (ey, ex)
 
 
 if __name__ == '__main__':
@@ -46,13 +26,13 @@ if __name__ == '__main__':
     # Part 1
     """
     timer.start()
-    G, start, end = preprocess(utils.read_str_lines())
+    G, start, end = process(utils.read_str_lines())
     print(nx.shortest_path_length(G, start, end))
     timer.stop()  # 65.14ms
     """
 
     # Part 2
     timer.start()
-    G, _, end = preprocess(utils.read_str_lines())
+    G, _, end = process(utils.read_str_lines())
     print(nx.multi_source_dijkstra(G, {node for node in G.nodes if G.nodes[node]['height'] == ord('a')}, end)[0])
     timer.stop()  # 91.56ms
